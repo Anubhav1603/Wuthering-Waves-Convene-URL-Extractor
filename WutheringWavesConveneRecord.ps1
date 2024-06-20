a38c3e4f71aeadcdf906997c71bbee7ad9be9da1
Anubhav1603 <anubhavsinha110@gmail.com>
2024-06-20 14:12:20 +0530

    [feat] Enhance script to offer JSON or URL copy based on user input , without needing of administrator perms

--- WutheringWavesConveneRecord.ps1
+++ WutheringWavesConveneRecord.ps1
@@ -1,79 +1,169 @@
-# got inspiration from https://gist.github.com/Luzefiru/19c0759bea1b9e7ef480bb39303b3f6c
+# this script is greatly inspired by https://gist.github.com/Luzefiru/19c0759bea1b9e7ef480bb39303b3f6c
+Add-Type -AssemblyName System.Web
 
-# Check if the process 'Wuthering Waves.exe' is running
-if (($process = Get-WmiObject Win32_Process -Filter "Name='Wuthering Waves.exe'" -ErrorAction SilentlyContinue)) {
-    
-    # Check if the executable path of the process is available
-    if ($process.ExecutablePath) {
-        
-        # Get the game installation path
-        $gamePath = Join-Path (Split-Path $process.ExecutablePath) "Client"
-        $gamePath = $gamePath -replace "\\Wuthering Waves.exe$", ""
+$64 = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*"
+$32 = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+Write-Output "Attempting to find URL automatically..."
 
-        # Define the log file path
-        $logFile = Join-Path $gamePath "Saved\Logs\Client.log"
+# Native
+try {
+    $gamePath = (Get-ItemProperty -Path $32, $64 | Where-Object { $_.DisplayName -like "*wuthering*" } | Select-Object InstallPath).PSObject.Properties.Value
+    if ((Test-Path ($gamePath + '\Client\Saved\Logs\Client.log') -or Test-Path($gamePath + '\Client\Binaries\Win64\ThirdParty\KrPcSdk_Global\KRSDKRes\KRSDKWebView\debug.log'))) {
+        $gachaLogPathExists = $true
+    }
+}
+catch {
+    $gamePath = $null
+    $gachaLogPathExists = $false
+}
 
-        # Check if the log file exists
-        if (-not (Test-Path $logFile)) {
-            Write-Host ""
-            Write-Host "The file '$logFile' does not exist." -ForegroundColor Red
-            Write-Host "Did you set your Game Installation Path properly?" -ForegroundColor Magenta
-            Read-Host "Press Enter to exit"
-            exit
+# MUI Cache
+if (!$gachaLogPathExists) {
+    $muiCachePath = "Registry::HKEY_CURRENT_USER\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\MuiCache"
+    $filteredEntries = (Get-ItemProperty -Path $muiCachePath).PSObject.Properties | Where-Object { $_.Value -like "*wuthering*" } | Where-Object { $_.Name -like "*client-win64-shipping.exe*" }
+    if ($filteredEntries.Count -ne 0) {
+        $gamePath = ($filteredEntries[0].Name -split '\\client\\')[0]
+        if ((Test-Path ($gamePath + '\Client\Saved\Logs\Client.log')) -or (Test-Path ($gamePath + '\Client\Binaries\Win64\ThirdParty\KrPcSdk_Global\KRSDKRes\KRSDKWebView\debug.log'))) {
+            $gachaLogPathExists = $true
         }
+    }
+}
 
+# Firewall 
+if (!$gachaLogPathExists) {
+    $firewallPath = "Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules"
+    $filteredEntries = (Get-ItemProperty -Path $firewallPath).PSObject.Properties | Where-Object { $_.Value -like "*wuthering*" } | Where-Object { $_.Name -like "*client-win64-shipping*" }
+    if ($filteredEntries.Count -ne 0) {
+        $gamePath = (($filteredEntries[0].Value -split 'App=')[1] -split '\\client\\')[0]
+        if ((Test-Path ($gamePath + '\Client\Saved\Logs\Client.log')) -or (Test-Path ($gamePath + '\Client\Binaries\Win64\ThirdParty\KrPcSdk_Global\KRSDKRes\KRSDKWebView\debug.log'))) {
+            $gachaLogPathExists = $true
+        }
+    }
+}
 
-        # Get the latest URL entry from the log file
-        $latestUrlEntry = Get-Content $logFile | Select-String "https://aki-gm-resources-oversea.aki-game.net" | Select-Object -Last 1
-
-        # Check if a matching URL entry is found
-        if ($latestUrlEntry) {
-            $urlPattern = 'url":"(.*?)"'
-            $url = [regex]::Match($latestUrlEntry, $urlPattern).Groups[1].Value
-
-            # Check if the URL is extracted successfully
-            if ($url) {
-
-                # Define the base URL
-                $baseUrl = "https://aki-gm-resources-oversea.aki-game.net/aki/gacha/index.html#/record?"
-
-                # Parse URL into key-value pairs
-                $urlParams = @{
-                    "svr_id"       = [regex]::Match($url, 'svr_id=([^&]+)').Groups[1].Value
-                    "player_id"    = [regex]::Match($url, 'player_id=([^&]+)').Groups[1].Value
-                    "lang"         = [regex]::Match($url, 'lang=([^&]+)').Groups[1].Value
-                    "gacha_id"     = [regex]::Match($url, 'gacha_id=([^&]+)').Groups[1].Value
-                    "gacha_type"   = [regex]::Match($url, 'gacha_type=([^&]+)').Groups[1].Value
-                    "svr_area"     = [regex]::Match($url, 'svr_area=([^&]+)').Groups[1].Value
-                    "record_id"    = [regex]::Match($url, 'record_id=([^&]+)').Groups[1].Value
-                    "resources_id" = [regex]::Match($url, 'resources_id=([^&]+)').Groups[1].Value
-                }
-
-                # Add base_url to urlParams
-                $urlParams.Add("base_url", $baseUrl)
-
-                # Convert to JSON format
-                $jsonOutput = ConvertTo-Json -InputObject $urlParams -Depth 5
-
-                # Copy JSON to clipboard
-                Set-Clipboard $jsonOutput
-
-                Write-Host ""
-                Write-Host "Data has been copied to clipboard. Please paste it in tracker." -ForegroundColor Green
+# Common Installation Paths
+if (!$gachaLogPathExists) {
+    $diskLetters = (Get-PSDrive).Name -match '^[a-z]$'
+    foreach ($diskLetter in $diskLetters) {
+        $gamePaths = @(
+            "$diskLetter`:\Wuthering Waves Game",
+            "$diskLetter`:\Wuthering Waves\Wuthering Waves Game",
+            "$diskLetter`:\Program Files\Epic Games\WutheringWavesj3oFh\Wuthering Waves Game"
+        )
+    
+        foreach ($gamePath in $gamePaths) {
+            if ((Test-Path ($gamePath + '\Client\Saved\Logs\Client.log')) -or (Test-Path ($gamePath + '\Client\Binaries\Win64\ThirdParty\KrPcSdk_Global\KRSDKRes\KRSDKWebView\debug.log')) ) {
+                $gamePath = $gamePath
+                $gachaLogPathExists = $true
+                break
             }
-            else {
-                Write-Host "No URL found." -ForegroundColor Yellow
-            }
+        }
+    
+        if ($gachaLogPathExists -or $gamePath) {
+            break
+        }
+    }
+}
+
+# Manual
+while (!$gachaLogPathExists) {
+    Write-Host "Game install location not found or log files missing. If you think that your installation directory is correct and it's still not working, please make issue on https://github.com/Anubhav1603/Wuthering-Waves-Convene-URL-Extractor. Otherwise, please enter the game install location path."
+    Write-Host 'Common install locations:'
+    Write-Host '  C:\Wuthering Waves' -ForegroundColor Yellow
+    Write-Host '  C:\Wuthering Waves\Wuthering Waves Game' -ForegroundColor Yellow
+    Write-Host '  C:\Program Files\Epic Games\WutheringWavesj3oFh' -ForegroundColor Yellow
+    $path = Read-Host "Path"
+    if ($path) {
+        $gamePath = $path
+        if ((Test-Path ($gamePath + '\Client\Saved\Logs\Client.log')) -or (Test-Path ($gamePath + '\Client\Binaries\Win64\ThirdParty\KrPcSdk_Global\KRSDKRes\KRSDKWebView\debug.log'))) {
+            $gachaLogPathExists = $true
         }
         else {
-            Write-Host ""
-            Write-Host "No matching entries found in the log file. Please open your Convene History first!" -ForegroundColor Red
+            Write-Host "Could not find log files. Did you set your game location properly or open your Convene History first?" -ForegroundColor Red
         }
     }
     else {
-        Write-Host "Process 'Wuthering Waves.exe' is not currently running!" -ForegroundColor Red
+        Write-Host "Invalid game location. Did you set your game location properly?" -ForegroundColor Red
+    }
+}
+
+$gachaLogPath = $gamePath + '\Client\Saved\Logs\Client.log'
+$debugLogPath = $gamePath + '\Client\Binaries\Win64\ThirdParty\KrPcSdk_Global\KRSDKRes\KRSDKWebView\debug.log'
+
+if (Test-Path $gachaLogPath) {
+    $gachaUrlEntry = Get-Content $gachaLogPath | Select-String -Pattern "https://aki-gm-resources-oversea\.aki-game\.(net|com)" | Select-Object -Last 1
+}
+else {
+    $gachaUrlEntry = $null
+}
+
+if (Test-Path $debugLogPath) {
+    $debugUrlEntry = Get-Content $debugLogPath | Select-String -Pattern '"#url": "(https://aki-gm-resources-oversea\.aki-game\.(net|com)[^"]*)"' | Select-Object -Last 1
+    $debugUrl = $debugUrlEntry.Matches.Groups[1].Value
+}
+else {
+    $debugUrl = $null
+}
+
+if ($gachaUrlEntry -or $debugUrl) {
+    if ($gachaUrlEntry) {
+        $urlToCopy = $gachaUrlEntry -replace '.*?(https://aki-gm-resources-oversea\.aki-game\.(net|com)[^"]*).*', '$1'
+    }
+    if ([string]::IsNullOrWhiteSpace($urlToCopy)) {
+        $urlToCopy = $debugUrl
+    }
+
+    if ([string]::IsNullOrWhiteSpace($urlToCopy)) {
+        Write-Host "Cannot find the convene history URL in both Client.log and debug.log! Please open your Convene History first!" -ForegroundColor Red
+    }
+    else {
+        # Prompt user for choice
+        Write-Host "Do you want to copy JSON data? (Y/N)" -ForegroundColor Yellow
+        $choice = Read-Host
+        if ($choice -eq "Y" -or $choice -eq "y") {
+            # Define the base URL
+            $baseUrl = "https://aki-gm-resources-oversea.aki-game.net/aki/gacha/index.html#/record?"
+ 
+            # Parse URL into key-value pairs
+            $urlParams = @{
+                "svr_id"       = [regex]::Match($urlToCopy, 'svr_id=([^&]+)').Groups[1].Value
+                "player_id"    = [regex]::Match($urlToCopy, 'player_id=([^&]+)').Groups[1].Value
+                "lang"         = [regex]::Match($urlToCopy, 'lang=([^&]+)').Groups[1].Value
+                "gacha_id"     = [regex]::Match($urlToCopy, 'gacha_id=([^&]+)').Groups[1].Value
+                "gacha_type"   = [regex]::Match($urlToCopy, 'gacha_type=([^&]+)').Groups[1].Value
+                "svr_area"     = [regex]::Match($urlToCopy, 'svr_area=([^&]+)').Groups[1].Value
+                "record_id"    = [regex]::Match($urlToCopy, 'record_id=([^&]+)').Groups[1].Value
+                "resources_id" = [regex]::Match($urlToCopy, 'resources_id=([^&]+)').Groups[1].Value
+            }
+ 
+            # Add base_url to urlParams
+            $urlParams.Add("base_url", $baseUrl)
+ 
+            # Convert to JSON format
+            $jsonOutput = ConvertTo-Json -InputObject $urlParams -Depth 5
+            
+            Write-Host ""
+            Write-Host "Json Data" -ForegroundColor Yellow
+            Write-Host $jsonOutput -ForegroundColor Yellow
+
+            # Copy JSON to clipboard
+            Set-Clipboard $jsonOutput
+ 
+            Write-Host ""
+            Write-Host "JSON data has been copied to clipboard. Please paste it in the tracker." -ForegroundColor Green
+        }
+        else {
+            # Copy URL to clipboard for any other choice including "N"
+            Set-Clipboard $urlToCopy
+ 
+            Write-Host ""
+            Write-Host "Convene Url: $urlToCopy" -ForegroundColor Yellow
+
+            Write-Host ""
+            Write-Host "URL has been copied to clipboard. Please paste it in the tracker." -ForegroundColor Green
+        }
     }
 }
 else {
-    Write-Host "Error: Unable to check the status of 'Wuthering Waves.exe'. Make sure game is running!" -ForegroundColor Red
+    Write-Host "Cannot find the convene history URL in both Client.log and debug.log! Please open your Convene History first!" -ForegroundColor Red
 }
